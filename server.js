6088eb6abdd6f3bfdf3ca14779730f495b57f961
Kevin Hu <pkvinhu@gmail.com>
2018-08-10 13:13:11 -0400

    finished react setup

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const { Player, Team, Country } = db.models;
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
+
+// app.use(express.static(path.join(process.cwd(), 'public')));
+
+const indexFile = path.join(process.cwd(), './public/index.html');
+const mainFile = path.join(process.cwd(), './public/main.js');
 
 const init = ()=> {
   db.syncAndSeed();
@@ -14,9 +21,18 @@ const init = ()=> {
 
 init();
 
+app.get('/', (req, res, next) => {
+  res.status(200).sendFile(indexFile);
+})
+
 app.get('/nba/players', (req, res, next)=> {
   Player.findAll({
   	include: [ Team ]
   })
   .then((players)=>{res.send(players)})
+})
+
+app.get('/nba/teams', (req, res, next)=> {
+  Team.findAll()
+  .then((teams)=> {res.send(teams)})
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+class Root extends Component {
+  render() {
+	return (
+	  <div>
+	  	<h1
+	  	  style={
+	  	  	textAlign='center',
+	  	  	backgroundColor='orange',
+	  	  	display='flex',
+	  	  	justifyContent='center'
+	  	  }
+	  	>Ball is Life</h1>
+	  </div>
+	);
+  }
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
